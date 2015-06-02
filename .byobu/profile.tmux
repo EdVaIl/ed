30d743917413d48b9360040aa2a3ae7402d917be
Ed Ilyin <ed.il@live.com>
2015-06-02 21:06:47 +0300

    makes byobu autolaunch as screen-256color

---
+++ .byobu/profile.tmux
@@ -0,0 +1,2 @@
+set -g default-terminal "screen-256color"
+source $BYOBU_PREFIX/share/byobu/profiles/tmux
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-set -x TERM xterm-256color
+#set -x TERM xterm-256color
 # Base16 Shell
 eval sh $HOME/.config/base16-shell/base16-solarized.dark.sh
+#set -x MC_SKIN $HOME/.mc/lib/mc-solarized/solarized.ini
 status --is-login; and status --is-interactive; and exec byobu-launcher
