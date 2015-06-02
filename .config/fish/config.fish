72483e4a8664a375347044e1e74d035131c668fb
Ed Ilyin <ed.il@live.com>
2015-06-02 04:59:48 +0300

    color tuning in cygwin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 set -x TERM xterm-256color
 # Base16 Shell
-eval sh $HOME/.config/base16-shell/base16-solarized.light.sh
+eval sh $HOME/.config/base16-shell/base16-solarized.dark.sh
 status --is-login; and status --is-interactive; and exec byobu-launcher
