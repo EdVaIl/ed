11fd89f07bda2667361b44ae13607ca2154a268f
Ed Ilyin <ed.il@live.com>
2015-07-21 11:43:18 +0300

    switching to base16 default color scheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,2 @@
-#set -x TERM xterm-256color
-# Base16 Shell
-eval sh $HOME/.config/base16-shell/base16-solarized.dark.sh
-#set -x MC_SKIN $HOME/.mc/lib/mc-solarized/solarized.ini
+eval sh $HOME/.config/base16-shell/base16-default.dark.sh
 status --is-login; and status --is-interactive; and exec byobu-launcher
