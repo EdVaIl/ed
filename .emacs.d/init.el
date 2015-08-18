646e4015a5ed707d21ad432a9290b65ada2c4b06
Ed Ilyin <ed.il@live.com>
2015-08-18 23:54:00 +0300

    adds colors to cursors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,7 @@
     evil-org
     evil-surround
     evil-terminal-cursor-changer
+    git-gutter
     ido-hacks
     magit
     powerline-evil
@@ -17,7 +18,7 @@
   )
   "A list of packages to ensure are installed at launch.")
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (require 'cl)
 ; method to check if all packages are installed
 (defun my-packages-installed-p ()
@@ -39,33 +40,37 @@
   (seethru 80)
 (powerline-evil-center-color-theme)
 (require 'evil-terminal-cursor-changer)
-  (setq evil-visual-state-cursor 'box) ; █
-  (setq evil-insert-state-cursor 'bar) ; ⎸
-  (setq evil-emacs-state-cursor 'hbar) ; _
+  (setq evil-emacs-state-cursor "violet")
+  (setq evil-insert-state-cursor '("blue" (bar . 2)))
+  (setq evil-motion-state-cursor "magenta")
+  (setq evil-normal-state-cursor "green")
+  (setq evil-operator-state-cursor '("cyan" evil-half-cursor))
+  (setq evil-replace-state-cursor '("red" hbar))
+  (setq evil-visual-state-cursor "orange")
 (require 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (require 'ido-hacks)
 (require 'evil-leader)
-(global-evil-leader-mode)
+  (global-evil-leader-mode)
 (require 'evil-org)
+(git-gutter:linum-setup)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (wombat)))
- '(custom-safe-themes
-   (quote
-    ("a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" default)))
+ '(display-time-24hr-format t)
+ '(display-time-default-load-average nil)
+ '(display-time-mode t)
  '(evil-mode t)
- '(fci-rule-color "#383838")
+ '(fringe-mode (quote (nil . 0)) nil (fringe))
  '(global-auto-complete-mode t)
  '(global-evil-surround-mode t)
+ '(global-git-gutter-mode t)
  '(global-linum-mode t)
  '(ido-enable-flex-matching t)
  '(ido-hacks-mode t)
@@ -76,33 +81,14 @@
  '(linum-delay t)
  '(menu-bar-mode nil)
  '(mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
+ '(save-place t nil (saveplace))
  '(scroll-bar-mode nil)
  '(server-mode t)
+ '(shell-file-name "bash")
  '(show-paren-mode t)
  '(show-paren-style (quote expression))
+ '(size-indication-mode t)
  '(tool-bar-mode nil)
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
  '(visible-bell t)
  '(xterm-mouse-mode t)
  '(yas-global-mode t nil (yasnippet)))
@@ -111,4 +97,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 143)))))
+ '(default ((t (:height 139 :family "DejaVu Sans Mono for Powerline")))))
