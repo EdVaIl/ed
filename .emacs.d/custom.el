a013e8e900495d9b449577ecd7bf5066195548da
Ed Ilyin <ed.il@live.com>
2015-08-23 03:13:59 +0300

    moves customization to custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,60 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(apropos-do-all t)
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes (quote (solarized)))
+ '(custom-file "~/.emacs.d/custom.el")
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(cygwin-mount-build-mount-table-asynch t)
+ '(cygwin-mount-cygwin-bin-directory "C:\\cygwin64\\bin")
+ '(display-time-24hr-format t)
+ '(display-time-default-load-average nil)
+ '(display-time-mode t)
+ '(evil-mode t)
+ '(frame-background-mode (quote dark))
+ '(global-auto-complete-mode t)
+ '(global-auto-revert-mode t)
+ '(global-evil-surround-mode t)
+ '(global-git-gutter-mode t)
+ '(global-linum-mode t)
+ '(helm-adaptive-mode t nil (helm-adaptive))
+ '(helm-autoresize-mode t)
+ '(helm-mode t)
+ '(helm-mode-fuzzy-match t)
+ '(inhibit-startup-screen t)
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(initial-scratch-message nil)
+ '(linum-delay t)
+ '(magit-push-always-verify t)
+ '(menu-bar-mode nil)
+ '(mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
+ '(org-startup-folded (quote content))
+ '(powerline-default-separator (quote wave))
+ '(save-place t nil (saveplace))
+ '(scroll-bar-mode nil)
+ '(server-mode t)
+ '(session-use-package t nil (session))
+ '(show-paren-mode t)
+ '(show-paren-style (quote expression))
+ '(size-indication-mode t)
+ '(solarized-termcolors 256)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(visible-bell t)
+ '(xterm-mouse-mode t)
+ '(yas-global-mode t nil (yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 108 :family "DejaVu Sans Mono for Powerline")))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,13 +5,19 @@
     company
     cygwin-mount
     diminish
+    evil
     evil-easymotion
+    evil-escape
     evil-god-state
     evil-leader
+    evil-matchit
+    evil-nerd-commenter
     evil-org
     evil-surround
     evil-terminal-cursor-changer
+    flycheck
     git-gutter
+    god-mode
     helm
     key-chord
     magit
@@ -19,8 +25,14 @@
     powershell
     projectile
     seethru
+    session
     ssh-agency
+    sublimity
+    tabbar
+    twittering-mode
+    web-beautify
     web-mode
+    windmove
     yasnippet
   )
   "A list of packages to ensure are installed at launch.")
@@ -45,7 +57,7 @@
       (package-install p))))
 
 (require 'seethru)
-  (seethru 90)
+  (seethru 100)
 
 (require 'powerline-evil-themes)
   (powerline-evil-center-color-theme)
@@ -95,6 +107,9 @@
       (setq evil-replace-state-cursor '("red" hbar))
       (setq evil-visual-state-cursor "orange")
 
+(require 'evil-easymotion)
+  ;(evilem-default-keybindings "SPC")
+	 
 (require 'key-chord)
   (key-chord-mode t)
   (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
@@ -111,6 +126,8 @@
 (require 'evil-leader)
   (global-evil-leader-mode)
 
+(require 'evil-matchit)
+
 (require 'evil-org)
 
 (require 'cygwin-mount)
@@ -122,61 +139,9 @@
 (require 'magit)
   (global-set-key (kbd "C-x g") 'magit-status)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(apropos-do-all t)
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(cua-mode t nil (cua-base))
- '(custom-enabled-themes (quote (solarized)))
- '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
- '(cygwin-mount-build-mount-table-asynch t)
- '(cygwin-mount-cygwin-bin-directory "C:\\cygwin64\\bin")
- '(display-time-24hr-format t)
- '(display-time-default-load-average nil)
- '(display-time-mode t)
- '(evil-mode t)
- '(frame-background-mode (quote dark))
- '(global-auto-complete-mode t)
- '(global-auto-revert-mode t)
- '(global-evil-surround-mode t)
- '(global-git-gutter-mode t)
- '(global-linum-mode t)
- '(helm-mode t)
- '(helm-mode-fuzzy-match t)
- '(inhibit-startup-screen t)
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(initial-scratch-message nil)
- '(linum-delay t)
- '(magit-push-always-verify t)
- '(menu-bar-mode nil)
- '(mouse-wheel-scroll-amount (quote (1 ((shift) . 5) ((control)))))
- '(org-startup-folded (quote content))
- '(powerline-default-separator (quote wave))
- '(save-place t nil (saveplace))
- '(scroll-bar-mode nil)
- '(server-mode t)
- '(show-paren-mode t)
- '(show-paren-style (quote expression))
- '(size-indication-mode t)
- '(solarized-termcolors 256)
- '(tool-bar-mode nil)
- '(visible-bell t)
- '(xterm-mouse-mode t)
- '(yas-global-mode t nil (yasnippet)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load "~/.emacs.d/custom.el")
 
 (set-terminal-parameter nil 'background-mode 'dark)
 (enable-theme 'solarized)
+(require 'session)
+  (add-hook 'after-init-hook 'session-initialize)
