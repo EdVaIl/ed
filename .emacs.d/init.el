1d817879a6f61cba7a8d2be4998f8aa66e0bee6c
Ed Ilyin <ed.il@live.com>
2015-08-25 23:35:00 +0300

    adds theme and transparency switches

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
 (defvar my-packages
   '(
-    color-theme-solarized
     company
     cygwin-mount
     diminish
@@ -22,13 +21,13 @@
     god-mode
     helm
     js2-mode
+    leuven-theme
     linum-relative
     magit
     monokai-theme
     powerline-evil
     powershell
     projectile
-    seethru
     session
     skewer-mode
     ssh-agency
@@ -64,65 +63,32 @@
 (require 'cygwin-mount)
   (cygwin-mount-activate)
 
+(require 'god-mode)
+  (defun my-update-cursor ()
+    (evil-set-cursor (if (or god-local-mode buffer-read-only)
+      'box
+      'bar)))
+  (add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+
 (require 'evil)
-  ;;; esc quits
-  ;(define-key evil-normal-state-map [escape] 'keyboard-quit)
-  ;(define-key evil-visual-state-map [escape] 'keyboard-quit)
-  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-  ;(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-  ;(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-  ;(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-  ;(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
 (require 'evil-easymotion)
-  (evilem-default-keybindings "SPC")
 	 
 (unless (display-graphic-p)
   (require 'evil-terminal-cursor-changer))
     (setq evil-emacs-state-cursor "violet")
     (setq evil-insert-state-cursor '("blue" (bar . 2)))
     (setq evil-motion-state-cursor "magenta")
-    (setq evil-normal-state-cursor "green")
+    (setq evil-normal-state-cursor '("green" box))
     (setq evil-operator-state-cursor '("cyan" evil-half-cursor))
     (setq evil-replace-state-cursor '("red" hbar))
     (setq evil-visual-state-cursor "orange")
 
-(require 'seethru)
-  (seethru 100)
-
-(require 'god-mode)
-  (global-set-key (kbd ",") 'god-local-mode)
-  (global-set-key (kbd "C-,") 'god-local-mode)
-  (global-set-key (kbd "C-x C-1") 'delete-other-windows)
-  (global-set-key (kbd "C-x C-2") 'split-window-below)
-  (global-set-key (kbd "C-x C-3") 'split-window-right)
-  (global-set-key (kbd "C-x C-0") 'delete-window)
-
-  (defun my-update-cursor ()
-    (setq cursor-type (if (or god-local-mode buffer-read-only)
-      'box
-      'bar)))
-  (add-hook 'god-mode-enabled-hook 'my-update-cursor)
-  (add-hook 'god-mode-disabled-hook 'my-update-cursor)
-
-  (defun c/god-mode-update-cursor ()
-    (let ((limited-colors-p (> 257 (length (defined-colors)))))
-      (cond (god-local-mode (progn
-        (set-face-background 'mode-line (if limited-colors-p "white" "#e9e2cb"))
-        (set-face-background 'mode-line-inactive (if limited-colors-p "white" "#e9e2cb"))))
-        (t (progn
-          (set-face-background 'mode-line (if limited-colors-p "black" "#0a2832"))
-          (set-face-background 'mode-line-inactive (if limited-colors-p "black" "#0a2832")))))))
-  (add-hook 'god-mode-enabled-hook 'c/god-mode-update-cursor)
-  (add-hook 'god-mode-disabled-hook 'c/god-mode-update-cursor)
-
-(vc-mode vc-mode)
-
 (require 'evil-god-state)
-  (evil-define-key 'normal global-map "," 'evil-execute-in-god-state)
   (add-hook 'evil-god-state-entry-hook (lambda () (diminish 'god-local-mode)))
-  (add-hook 'evil-god-state-exit-hook (lambda () (diminish-undo 'god-local-mode)))
-  (evil-define-key 'god global-map [escape] 'evil-god-state-bail)
+  (add-hook 'evil-god-state-exit-hook
+	    (lambda () (diminish-undo 'god-local-mode)))
 
 (require 'web-mode)
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
@@ -140,9 +106,10 @@
   (git-gutter:linum-setup)
 
 (require 'magit)
-  (global-set-key (kbd "C-x g") 'magit-status)
 
 (load "~/.emacs.d/custom.el")
+(load "~/.emacs.d/theme.el")
+(load "~/.emacs.d/keys.el")
 
 (require 'diminish)
   (defmacro my-diminish (package mode unicode ascii)
@@ -157,10 +124,6 @@
   (my-diminish "isearch"     isearch-mode     "ⓢ" "σ")
   (my-diminish "undo-tree"   undo-tree-mode   "ⓤ" "θ")
   (my-diminish "yasnippet"   yas-minor-mode   "ⓨ" "υ")
-
-; solarized
-(set-terminal-parameter nil 'background-mode frame-background-mode)
-(enable-theme 'monokai)
 
 (require 'powerline-evil-themes)
   (powerline-evil-center-color-theme)
