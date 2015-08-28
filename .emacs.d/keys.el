f9be5fa9ca3be21b98acc5719bddff74e4c59977
Ed Ilyin <ed.il@live.com>
2015-08-28 20:53:22 +0300

    many changes to ~/.emacs.d/

--- .emacs.d/keys.el
+++ .emacs.d/keys.el
@@ -10,12 +10,23 @@
 (evil-define-key 'motion global-map "," 'evil-execute-in-god-state)
 (evil-define-key 'normal global-map "," 'evil-execute-in-god-state)
 (evilem-default-keybindings "SPC")
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cl" 'org-store-link)
 ;(global-set-key (kbd ",") 'god-local-mode)
 (global-set-key (kbd "C-,") 'god-local-mode)
-(global-set-key (kbd "C-c C-t") 'my-theme-toggle)
-(global-set-key (kbd "C-c C-a") (lambda () (interactive)
-  (set-frame-parameter (selected-frame) 'alpha
-    (if (eql (frame-parameter (selected-frame) 'alpha) 75) 100 75))))
+(evil-leader/set-key
+  "b" 'my-theme-toggle
+  "s" '(lambda () (interactive)
+	 (set-frame-parameter
+	  (selected-frame) 'alpha (if (eql (frame-parameter (selected-frame) 'alpha) 75)
+				      100
+				    75))))
+;(global-set-key (kbd "C-c C-t") 'my-theme-toggle)
+;(global-set-key (kbd "C-c C-a") (lambda () (interactive)
+;  (set-frame-parameter (selected-frame) 'alpha
+;    (if (eql (frame-parameter (selected-frame) 'alpha) 75) 100 75))))
 (global-set-key (kbd "C-x C-0") 'delete-window)
 (global-set-key (kbd "C-x C-1") 'delete-other-windows)
 (global-set-key (kbd "C-x C-2") 'split-window-below)
--- .emacs.d/theme.el
+++ .emacs.d/theme.el
@@ -1,16 +1,19 @@
 (require 'powerline)
+
 (defun my-theme ()
   (if (string= frame-background-mode 'light)
       (load-theme 'leuven)
     (load-theme 'monokai))
   (powerline-reset))
+
 (defun my-theme-toggle ()
   (interactive)
   (evil-god-state-bail)
   (if (string= frame-background-mode 'light)
-      (setq frame-background-mode 'dark)
-    (setq frame-background-mode 'light))
+      (progn (setq frame-background-mode 'dark) (disable-theme 'leuven))
+    (progn (setq frame-background-mode 'light) (disable-theme 'monokai)))
   (frame-set-background-mode (selected-frame))
   (set-terminal-parameter nil 'background-mode frame-background-mode)
   (my-theme))
+
 (my-theme)
